7a03cccebeca4e6f8052a0cd1566ab9714d82576
Matteo Scanferla <m.scanferla@eggonemail.com>
2023-09-20 21:29:00 +0200

    [APWI-10]

--- pw-angular/src/app/app-routing.module.ts
+++ pw-angular/src/app/app-routing.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guard/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
 
   {path:"", redirectTo:"login", pathMatch:"full"},
   {path:"login", component:LoginComponent },
+  {path:"home", component:HomeComponent,  /* canActivate:[authGuard] */},
 
 ];
 
-
-//NOTE -
-//  canActivate:[authGuard]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

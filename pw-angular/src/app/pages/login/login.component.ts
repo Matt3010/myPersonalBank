0ae22a2b7e66b784c78eaac65f0756e93ebb4e38
Matteo Scanferla <m.scanferla@eggonemail.com>
2023-09-21 08:47:27 +0200

    added toastr

--- pw-angular/src/app/pages/login/login.component.ts
+++ pw-angular/src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Login } from 'src/app/interfaces/login';
 import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   templateUrl: './login.component.html',
@@ -9,7 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent {
 
-  constructor(private authSrv: AuthService, private route: Router){
+  constructor(private authSrv: AuthService, private route: Router, private toastr: ToastrService) {
   }
 
   loginErrors: any | null = null
@@ -18,10 +20,12 @@ export class LoginComponent {
     this.authSrv.login(loginPayload).subscribe(
       (res)=>{
           //TODO - Service popup loggato
+          this.toastr.success("You are correctly logged in!", "My personal Bank")
           this.route.navigateByUrl("/home")
         },
       (err)=>{
         console.log(err)
+          this.toastr.error("Sorry! An error has occurred", "My personal Bank")
           this.loginErrors = err
       }
     )
